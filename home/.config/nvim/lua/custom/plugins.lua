1fd12b6dd53accd3a4c94a684e174150eca51845
Chris Patti <feoh@feoh.org>
2023-01-20 10:36:40 -0500

    Add Glow plugin for markdown preview

--- home/.config/nvim/lua/custom/plugins.lua
+++ home/.config/nvim/lua/custom/plugins.lua
@@ -1,4 +1,5 @@
 return function(use)
+  use {"ellisonleao/glow.nvim"}
   use {'mfussenegger/nvim-dap'}
   use {'mfussenegger/nvim-dap-python'}
   require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
