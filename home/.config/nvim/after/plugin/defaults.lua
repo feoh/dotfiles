487ba48e63f5d89f501d8b413f03f1868eba4d60
Chris Patti <feoh@feoh.org>
2023-01-23 12:43:05 -0500

    Rejigger dap debugging keybindings, add optional dap-ui.

--- home/.config/nvim/after/plugin/defaults.lua
+++ home/.config/nvim/after/plugin/defaults.lua
@@ -1,12 +1,17 @@
-vim.keymap.set('n','<F5>',function() require('dap').continue() end)
-vim.keymap.set('n','<F10>',function() require('dap').step_over() end)
-vim.keymap.set('n','<F11>',function() require('dap').step_into() end)
-vim.keymap.set('n','<F12>',function() require('dap').step_out() end)
+-- nvim-dap debug key mappings
+vim.keymap.set('n','<Leader>dc',function() require('dap').continue() end)
+vim.keymap.set('n','<Leader>do',function() require('dap').step_over() end)
+vim.keymap.set('n','<Leader>di',function() require('dap').step_into() end)
+vim.keymap.set('n','<Leader>du',function() require('dap').step_out() end)
 vim.keymap.set('n','<Leader>b',function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n','<Leader>B',function() require('dap').set_breakpoint() end)
 vim.keymap.set('n','<Leader>lp',function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 vim.keymap.set('n','<Leader>dr',function() require('dap').repl.open() end)
 vim.keymap.set('n','<Leader>dl',function() require('dap').run_last() end)
+vim.keymap.set('n','<Leader>tm',function() require('dap-python').test_method() end)
+vim.keymap.set('n','<Leader>dut',function() require("dapui").toggle() end)
+
+-- Blogging
 vim.keymap.set('i','<Leader>idt', "<cmd>pu=strftime('%Y-%m-%d %H:%M:%S')<CR>")
 
 -- Options
--- home/.config/nvim/lua/custom/plugins.lua
+++ home/.config/nvim/lua/custom/plugins.lua
@@ -2,7 +2,16 @@ return function(use)
   use {"ellisonleao/glow.nvim"}
   use {'mfussenegger/nvim-dap'}
   use {'mfussenegger/nvim-dap-python'}
+  use {'theHamsta/nvim-dap-virtual-text'}
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+
   require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+  require("nvim-dap-virtual-text").setup {
+    enabled = true,
+    commented = true,
+  }
+  require("dapui").setup()
+
 end
 
 
